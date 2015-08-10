e71d513fbeb1481662b2c95dc93a4713c3e253b0
Felix Palludan Hargreaves <hejfelix@gmail.com>
2015-08-10 11:33:25 +0200

    Beta reduction

--- src/main/scala/it/vigtig/lambda/Parser.scala
+++ src/main/scala/it/vigtig/lambda/Parser.scala
@@ -9,36 +9,35 @@ import scala.language.postfixOps
  */
 trait Parser extends RegexParsers with PackratParsers {
   import LambdaAST._
-  
-    lazy val identifier:PackratParser[String]  = 
+
+    lazy val identifier:PackratParser[String]  =
       """[a-zA-Z]+""".r ^^ {_.toString}
-    
+
     lazy val TERM:PackratParser[Term] = LABSTR | APP | ID | PEXPR
-    
-    lazy val PEXPR:PackratParser[Term] = "(" ~> TERM <~ ")" ^^ 
-      { term => term }
-      
-    lazy val ID:PackratParser[Id] = 
+
+    lazy val PEXPR:PackratParser[Term] = "(" ~> TERM <~ ")" 
+
+    lazy val ID:PackratParser[Id] =
       identifier ^^ Id
-    
-    lazy val LABSTR:PackratParser[Abstraction] = 
+
+    lazy val LABSTR:PackratParser[Abstraction] =
       ID ~ "." ~ TERM ^^
         { case id ~ _ ~ term => Abstraction(id,term) }
-    
-    lazy val APP:PackratParser[Application] = 
+
+    lazy val APP:PackratParser[Application] =
       TERM ~ TERM ^^
         { case t1 ~ t2 => Application(t1,t2) }
-    
+
 }
 
 
 object LambdaAST {
-  
-  abstract trait Term 
+
+  abstract trait Term
   case class Id(id:String) extends Term
   case class Abstraction(id:Id,body:Term) extends Term
   case class Application(left:Term,right:Term) extends Term
-  
+
 }
 
 object TestLambdaParser extends Parser with App {
