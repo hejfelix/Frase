261de6c7a6d3297cab9a3dc1bc5664e5c928a402
Felix Palludan Hargreaves <hejfelix@gmail.com>
2015-08-11 19:05:04 +0200

    Added named terms

--- src/main/scala/it/vigtig/lambda/Interpreter.scala
+++ src/main/scala/it/vigtig/lambda/Interpreter.scala
@@ -5,52 +5,55 @@ package it.vigtig.lambda
  */
 object Interpreter extends Parser with App {
   import LambdaAST._
-  
+
   val TEST1 = "( y . (x . y z x ) a) b"
   val TEST2 = "a b c d"
-  
-  def prettyStr(t:Term):String = t match {
-    case Application(a,b) => 
-      s"(${prettyStr(a)} ${prettyStr(b)})"
-    case Abstraction(Id(x),b) => 
-      s"λ$x -> ${prettyStr(b)}"
-    case Id(x) => x
-  }
-  
-  parseAll(TERM, TEST1) match {
-    case Success(lup,_) => 
-      println(prettyStr(lup))
-      println("  beta-reduction ->")
-      println(prettyStr(reduce(lup)))
+  val TESTNAMED = "a = ( y . (x . y z x ) a) b"
+
+  parseAll(PRGM, TESTNAMED) match {
+    case Success(lup, _) =>
+      println(lup.map(prettyStr).mkString)
+      println("  beta-reduction --->")
+      println( lup.map( (prettyStr _).compose(reduce)).mkString )
     case x => println(x)
   }
-  
-  def interpret(term:Term) = betaReduction(term)
-  
-  def reduce(t:Term):Term = t match {
+
+  def interpret(term: Term) = betaReduction(term)
+
+  def reduce(t: Term): Term = t match {
     case Id(_) => t
-    case Application(Abstraction(id,body),rhs) => 
-      reduce(betaReduce(rhs,id,body))
-    case Application(t,y) => Application(reduce(t),reduce(y))
-    case Abstraction(a,b) => Abstraction(a,reduce(b))
-    case _ => t
+    case Named(id,body) => Named(id,reduce(body))
+    case Application(Abstraction(id, body), rhs) =>
+      reduce(betaReduce(rhs, id, body))
+    case Application(t, y) => Application(reduce(t), reduce(y))
+    case Abstraction(a, b) => Abstraction(a, reduce(b))
+    case _                 => t
   }
-  
-  def fixPoint[T](t:T)(p:T => T):T =
-    if(p(t) != t)
+
+  def prettyStr(t: Term): String = t match {
+    case Application(a, b) =>
+      s"(${prettyStr(a)} ${prettyStr(b)})"
+    case Abstraction(Id(x), b) =>
+      s"$x . ${prettyStr(b)}"
+    case Id(x)              => x
+    case Named(Id(x), term) => s"$x = ${prettyStr(term)}"
+  }
+
+  def fixPoint[T](t: T)(p: T => T): T =
+    if (p(t) != t)
       fixPoint(p(t))(p)
     else
       t
-      
-  def betaReduction(t:Term):Term = fixPoint(t)(reduce)
-  
-  def betaReduce(newX:Term, oldX:Id, in:Term): Term = in match {
+
+  def betaReduction(t: Term): Term = fixPoint(t)(reduce)
+
+  def betaReduce(newX: Term, oldX: Id, in: Term): Term = in match {
     case `oldX` => newX
-    case Id(_) => in
-    case Abstraction(id,body) => 
+    case Id(_)  => in
+    case Abstraction(id, body) =>
       Abstraction(id, betaReduce(newX, oldX, body))
-    case Application(l,r) => 
+    case Application(l, r) =>
       Application(betaReduce(newX, oldX, l), betaReduce(newX, oldX, r))
   }
- 
+
 }
--- src/main/scala/it/vigtig/lambda/Parser.scala
+++ src/main/scala/it/vigtig/lambda/Parser.scala
@@ -10,39 +10,48 @@ import scala.language.postfixOps
 trait Parser extends RegexParsers with PackratParsers {
   import LambdaAST._
 
-    lazy val identifier:PackratParser[String]  =
-      """[a-zA-Z]+""".r ^^ {_.toString}
+  lazy val identifier: PackratParser[String] =
+    """[a-zA-Z]+""".r ^^ { _.toString }
 
-    lazy val TERM:PackratParser[Term] = LABSTR | APP | ID | PEXPR
+  lazy val PRGM:PackratParser[List[Term]] = (NAMED | TERM).*
+  
+  lazy val NAMED:PackratParser[Named] = 
+    ID ~ ("=" ~> TERM) ^^
+     { case id ~ body => Named(id,body) }
+  
+  lazy val TERM: PackratParser[Term] = LABSTR | APP | ID | PEXPR
 
-    lazy val PEXPR:PackratParser[Term] = "(" ~> TERM <~ ")" 
+  lazy val PEXPR: PackratParser[Term] = "(" ~> TERM <~ ")"
 
-    lazy val ID:PackratParser[Id] =
-      identifier ^^ Id
+  lazy val ID: PackratParser[Id] =
+    identifier ^^ Id
 
-    lazy val LABSTR:PackratParser[Abstraction] =
-      ID ~ "." ~ TERM ^^
-        { case id ~ _ ~ term => Abstraction(id,term) }
+  lazy val LABSTR: PackratParser[Abstraction] =
+    ID ~ "." ~ TERM ^^
+      { case id ~ _ ~ term => Abstraction(id, term) }
 
-    lazy val APP:PackratParser[Term] =
-      TERM ~ ID ^^
-        { case t1 ~ t2 => Application(t1,t2)}
+  lazy val APP: PackratParser[Term] =
+    TERM ~ ID ^^
+      { case t1 ~ t2 => Application(t1, t2) }
 
+  
+  
 }
-
 
 object LambdaAST {
 
   abstract trait Term
-  case class Id(id:String) extends Term
-  case class Abstraction(id:Id,body:Term) extends Term
-  case class Application(left:Term,right:Term) extends Term
+  case class Id(id: String) extends Term
+  case class Abstraction(id: Id, body: Term) extends Term
+  case class Application(left: Term, right: Term) extends Term
+  case class Named(id:Id,body:Term) extends Term
+  
 
 }
 
 object TestLambdaParser extends Parser with App {
   parseAll(TERM, "( x . y z) a") match {
-    case Success(lup,_) => println(lup)
-    case x => println(x)
+    case Success(lup, _) => println(lup)
+    case x               => println(x)
   }
 }
