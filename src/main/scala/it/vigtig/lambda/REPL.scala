eec4c9164d79f2099b2ec5acb65785e36296d070
Felix Palludan Hargreaves <hejfelix@gmail.com>
2015-08-18 11:32:35 +0200

    Added REPL

---
+++ src/main/scala/it/vigtig/lambda/REPL.scala
@@ -0,0 +1,21 @@
+package it.vigtig.lambda
+/**
+ * @author Hargreaves
+ */
+object REPL 
+extends Parser
+with InterpreterLike {
+  def main(args: Array[String]) = loop()
+ 
+  def loop():Unit = {
+      val exprSrc = io.StdIn.readLine("Frase>")
+      parseAll(LINE,exprSrc) match {
+        case Success(expr, _) => 
+          println("Parsed: " + prettyStr(expr))
+          println("Evaluated: "+prettyStr(interpret(expr)))
+          println()
+        case err: NoSuccess   => println(err)
+      }
+      loop()
+  }
+}
