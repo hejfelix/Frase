8f9241642ce5df106531bc0e153e09088ffd5a74
Felix Palludan Hargreaves <hejfelix@gmail.com>
2015-08-20 09:48:21 +0200

    Initial Hindley Milner type system implementation. Rules for [Var], [App], [Abstr]

--- src/main/scala/it/vigtig/lambda/Interpreter.scala
+++ src/main/scala/it/vigtig/lambda/Interpreter.scala
@@ -6,50 +6,48 @@ package it.vigtig.lambda
 
 trait InterpreterLike {
   import LambdaAST._
-  def interpret(t: Term)(context:Map[Id,Term] = Map()): Term 
-    = fixPoint(t)(evalStep(context))
+  def interpret(t: Term)(context: Map[Id, Term] = Map()): Term = fixPoint(t)(evalStep(context))
 
-   def show[T](t:T):T = {
+  def show[T](t: T): T = {
     println(t)
     t
   }
-  
-  def evalStep(context:Map[Id,Term])(t:Term) = 
-     fixPoint(resolve(t)(context))(reducer)
-  
 
-  def resolve(t:Term)(context:Map[Id,Term]):Term = context.foldLeft(t)((a,b) => substitute(a)(b))
-  
+  def evalStep(context: Map[Id, Term])(t: Term) =
+    fixPoint(resolve(t)(context))(reducer)
+
+  def resolve(t: Term)(context: Map[Id, Term]): Term = context.foldLeft(t)((a, b) => substitute(a)(b))
+
   def size(t: Term): Int = t match {
-    case Id(_)                        => 1
-    case Named(id, body)              => 1 + size(id) + size(body)
-    case Applic(t, y)                 => 1 + size(t) + size(y)
-    case Abstr(a, b)                  => 1 + size(a) + size(b)
-    case _                            => 1
+    case Id(_)           => 1
+    case Named(id, body) => 1 + size(id) + size(body)
+    case Applic(t, y)    => 1 + size(t) + size(y)
+    case Abstr(a, b)     => 1 + size(a) + size(b)
+    case _               => 1
   }
 
-  def builtIns:PartialFunction[Term,Term] = {
-    case Applic(Applic(Id("=="),a),b) => Bit(a==b)
-    case Applic(Applic(Id("*"),Integer(x)),Integer(y)) => Integer(x*y)
-    case Applic(Applic(Id("+"),Integer(x)),Integer(y)) => Integer(x+y)
-    case Applic(Applic(Id("-"),Integer(x)),Integer(y)) => Integer(x-y)
-    case Applic(Applic(Bit(p),yes),no) => if(p) yes else no
-    case Applic(Applic(Id("%"),Integer(a)),Integer(b)) => Integer(a%b)
-    case Applic(Applic(Id("<="),Integer(a)),Integer(b)) => Bit(a<=b)
+  val App = Applic
+  def builtIns: PartialFunction[Term, Term] = {
+    case App(App(Id("=="), a), b)                   => Bit(a == b)
+    case App(App(Id("*"), Integer(x)), Integer(y))  => Integer(x * y)
+    case App(App(Id("+"), Integer(x)), Integer(y))  => Integer(x + y)
+    case App(App(Id("-"), Integer(x)), Integer(y))  => Integer(x - y)
+    case App(App(Bit(p), yes), no)                  => if (p) yes else no
+    case App(App(Id("%"), Integer(a)), Integer(b))  => Integer(a % b)
+    case App(App(Id("<="), Integer(a)), Integer(b)) => Bit(a <= b)
   }
 
   def reducer = {
-      def betaReduce:PartialFunction[Term,Term] = builtIns orElse {
+    def betaReduce: PartialFunction[Term, Term] = builtIns orElse {
       case Named(id, body)              => Named(id, betaReduce(body))
       case Applic(Abstr(id, body), rhs) => betaReduce(substitute(body)(id -> rhs))
       case Applic(t, y)                 => Applic(betaReduce(t), betaReduce(y))
       case Abstr(a, b)                  => Abstr(a, betaReduce(b))
-      case i@Id(_)                      => i
+      case i @ Id(_)                    => i
       case t                            => t
     }
-      betaReduce
+    betaReduce
   }
- 
 
   def prettyStr(t: Term): String = t match {
     case Applic(a @ Id(_), b) => s"${prettyStr(a)} ${prettyStr(b)}"
@@ -63,7 +61,7 @@ trait InterpreterLike {
     case Bit(b)               => b.toString
   }
 
-  def fixPoint[T](t: T)(p: T => T): T ={
+  def fixPoint[T](t: T)(p: T => T): T = {
     if (p(t) != t)
       fixPoint(p(t))(p)
     else
@@ -76,20 +74,20 @@ trait InterpreterLike {
     case Applic(a, b)    => freeVars(a) ++ freeVars(b)
     case Empty           => Set()
     case Named(id, term) => freeVars(term)
-    case _         => Set()
+    case _               => Set()
   }
 
   //Capture-avoiding substitution
   def substitute(t: Term)(label: (Id, Term)): Term = (t, label) match {
-    case (Empty,_) => Empty
+    case (Empty, _)                => Empty
     case (i: Id, (j, k)) if i == j => k
     case (i: Id, _)                => i
-    case (a:Atom, _)               => a
+    case (a: Atom, _)              => a
     case (Applic(a, b), _)         => Applic(substitute(a)(label), substitute(b)(label))
     case (Abstr(id, body), (x, y)) if id != x && !(freeVars(y)(id)) =>
       Abstr(id, substitute(body)(label))
     case (a @ Abstr(_, _), _) => a
-    case _ => t
+    case _                    => t
   }
 
 }
@@ -120,7 +118,7 @@ object Interpreter extends Parser with InterpreterLike with App {
     fib 6
     fib 7
     """
-  
+
   val TESTSUB = """
     x = a b c
     y = d e f
@@ -128,7 +126,7 @@ object Interpreter extends Parser with InterpreterLike with App {
     
     x y z
     """
-  
+
   val TESTFAC = """
     fac = n . (<= n 1) (1) (* (n) (fac (- n 1)))
     
@@ -139,30 +137,28 @@ object Interpreter extends Parser with InterpreterLike with App {
     fac 5
     fac 6
     """
-  
+
   val label = Id("y") -> Id("y")
 
   parseAll(PRGM, TESTFIB) match {
     case Success(lup, _) =>
-      val (nameds,unnameds) = lup filter (_!=Empty) partition {
+      val (nameds, unnameds) = lup filter (_ != Empty) partition {
         case n: Named => true
         case _        => false
       }
-      
 
-      val dict:Map[Id,Term] = nameds.map {
+      val dict: Map[Id, Term] = nameds.map {
         case Named(a, b) => a -> b
       }.toMap
 
-      println("program:\n"+(lup filter (_!=Empty) map prettyStr).mkString("\n"))
+      println("program:\n" + (lup filter (_ != Empty) map prettyStr).mkString("\n"))
       println()
-      
-      println("Evaluating...\n"+unnameds.map(t => {
+
+      println("Evaluating...\n" + unnameds.map(t => {
         val res = interpret(t)(dict)
         interpret(res)()
       }).map(prettyStr).mkString("\n"))
-      
-      
+
     case x => println(x)
   }
 
