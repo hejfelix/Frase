e82a9cdaacfb410fe1f1785dbfdd9ff89eadb20e
Felix Palludan Hargreaves <hejfelix@gmail.com>
2015-08-21 10:03:43 +0200

    Moved ASTGenerator trait to separate file

---
+++ src/test/scala/ASTGenerators.scala
@@ -0,0 +1,28 @@
+
+
+import org.scalacheck.Arbitrary
+import org.scalacheck.Arbitrary.arbitrary
+import org.scalacheck.Gen
+
+import it.vigtig.lambda.ASTLike
+/*
+ * @author Hargreaves
+ */
+trait ASTGenerators  {
+  self:ASTLike =>
+  implicit def id = Arbitrary { Gen.alphaChar map (x => Id("" + x)) }
+  val int: Gen[Atom] = arbitrary[Int] map Integer
+  val float: Gen[Atom] = arbitrary[Float] map Floating
+
+  implicit def bool: Arbitrary[Atom] = Arbitrary { arbitrary[Boolean] map Bit }
+
+  implicit def atom = Arbitrary { Gen.oneOf(id, int, float, bool) }
+  
+  implicit lazy val idStr = 
+    Arbitrary {
+    for {
+      x <- Gen.alphaLowerChar 
+      y <- Gen.alphaStr if x.isLetter
+    } yield {x + y}
+  }
+}
--- src/test/scala/FraseGenerator.scala
+++ src/test/scala/FraseGenerator.scala
@@ -1,12 +1,6 @@
 /**
  * @author Felix
  */
-import org.scalacheck.Arbitrary
-import org.scalacheck.Arbitrary.arbBool
-import org.scalacheck.Arbitrary.arbFloat
-import org.scalacheck.Arbitrary.arbInt
-import org.scalacheck.Arbitrary.arbitrary
-import org.scalacheck.Gen
 import org.scalatest.Matchers.convertToAnyShouldWrapper
 import org.scalatest.PropSpec
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
@@ -14,25 +8,6 @@ import org.scalatest.prop.GeneratorDrivenPropertyChecks
 import it.vigtig.lambda.ASTLike
 import it.vigtig.lambda.InterpreterLike
 import it.vigtig.lambda.ParserLike
-
-trait ASTGenerators  {
-  self:ASTLike =>
-  implicit def id = Arbitrary { Gen.alphaChar map (x => Id("" + x)) }
-  val int: Gen[Atom] = arbitrary[Int] map Integer
-  val float: Gen[Atom] = arbitrary[Float] map Floating
-
-  implicit def bool: Arbitrary[Atom] = Arbitrary { arbitrary[Boolean] map Bit }
-
-  implicit def atom = Arbitrary { Gen.oneOf(id, int, float, bool) }
-  
-  implicit lazy val idStr = 
-    Arbitrary {
-    for {
-      x <- Gen.alphaLowerChar 
-      y <- Gen.alphaStr if x.isLetter
-    } yield {x + y}
-  }
-}
 
 class FraseGenerator extends PropSpec
     with InterpreterLike
