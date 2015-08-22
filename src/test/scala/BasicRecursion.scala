07774ae415d076ea56ec4d5eceb9686c2c251344
Felix Palludan Hargreaves <hejfelix@gmail.com>
2015-08-22 19:15:42 +0200

    Cleanup of tests + additional tests for recursion

---
+++ src/test/scala/BasicRecursion.scala
@@ -0,0 +1,51 @@
+/**
+ * @author Felix
+ */
+import org.scalatest.Matchers.convertToAnyShouldWrapper
+import org.scalatest.PropSpec
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
+import it.vigtig.lambda.AST
+import it.vigtig.lambda.InterpreterLike
+import it.vigtig.lambda.ParserLike
+import org.scalacheck.Gen
+
+class BasicRecursion extends PropSpec
+    with InterpreterLike
+    with ParserLike
+    with ASTGenerators
+    with GeneratorDrivenPropertyChecks {
+  import AST._
+
+  def parseProgramTest(l: String)(b: Term => Unit) = {
+    interpretProgram(l) match {
+      case Some(ts) => ts map (t => b(interpret(t)()))
+      case None     => ???
+    }
+  }
+
+  def fib(n: Int): Int = if (n < 2) n else fib(n - 1) + fib(n - 2)
+  val FIB_FUNC = 
+"""
+fib = n . (<= n 1) (n) ((+ (fib (- n 2)) (fib (- n 1))))
+"""
+  
+  property("Fibonacci sequence") {
+    forAll(Gen.choose(0, 15)) {
+      (n: Int) => parseProgramTest(FIB_FUNC + s"fib $n")(_ shouldBe Integer(fib(n)))
+    }
+  }
+  
+  
+  def fac(n:Int) = (1 /: (1 to n))(_*_) 
+  val FAC_FUNC = 
+"""
+fac = n . (<= n 1) (1) (* (n) (fac (- n 1)))
+"""
+  
+  property("Factorial function") {
+    forAll(Gen.choose(0, 15)) {
+      (n: Int) => parseProgramTest(FAC_FUNC + s"fac $n")(_ shouldBe Integer(fac(n)))
+    }
+  }
+  
+}
