fd69ab7197bcd21b4ac3fef142ee8123d8bf842b
Felix Palludan Hargreaves <hejfelix@gmail.com>
2015-09-05 21:59:43 +0200

    Added tests for pattern matching

--- src/test/scala/it/vigtig/frase/spectest/PatternMatching.scala
+++ src/test/scala/it/vigtig/frase/spectest/PatternMatching.scala
@@ -17,7 +17,7 @@ with GeneratorDrivenPropertyChecks {
 
   def parseProgramTest(l: String)(b: Term => Unit) = {
     interpretProgram(l) match {
-      case Some(ts) => ts foreach (t => b(interpret(t)()))
+      case Some(ts) => ts.filter(_!=Empty) foreach (t => b(interpret(t)()))
       case None     => ???
     }
   }
@@ -25,16 +25,31 @@ with GeneratorDrivenPropertyChecks {
   val LIST =
     """
 set List = Nil or Cons x:Int,xs:List
+
 head = (Cons x xs) . x
 tail = (Cons x xs) . xs
-head (Cons 42 Nil)
+
+size = Nil . 0
+size = (Cons x xs) . (+ 1 (size xs))
     """
 
   property("Head") {
-     parseAll(PRGM,LIST) match {
-       case Success(r,_) => println(r.mkString("\n"))
-       case _ =>
-     }
+    parseProgramTest(LIST+s"\nhead (Cons 42 Nil)")(_ shouldBe Integer(42))
   }
+
+  property("Tail") {
+    parseProgramTest(LIST+s"\ntail (Cons 42 Nil)")(_ shouldBe SetId("Nil"))
+  }
+
+
+  property("size Nil should be 0") {
+    parseProgramTest(LIST+s"\nsize Nil")(_ shouldBe Integer(0))
+  }
+
+
+  property("Size") {
+    parseProgramTest(LIST+s"\nsize (Cons 42 (Cons 1337 (Cons 9000 Nil)))")(_ shouldBe Integer(3))
+  }
+
 
 }
