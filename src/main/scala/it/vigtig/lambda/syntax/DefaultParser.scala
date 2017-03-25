e336200e755a923f600c918c193e5834544ba4c3
Felix Palludan Hargreaves <hejfelix@gmail.com>
2017-03-25 11:38:12 +0100

    Missed file (thx gitkraken..)

--- src/main/scala/it/vigtig/lambda/syntax/DefaultParser.scala
+++ src/main/scala/it/vigtig/lambda/syntax/DefaultParser.scala
@@ -1,54 +1,66 @@
 package it.vigtig.lambda.syntax
 
+import it.vigtig.lambda.syntax.AST._
+
 import scala.util.parsing.combinator.{PackratParsers, Parsers}
 import scala.util.parsing.input.{Position, Reader}
 
-sealed trait Fragment {
+object AST {
+  sealed trait Fragment {
 
-  def pretty: String = this match {
-    case Named(lhs, rhs)             => s"${lhs.pretty} = ${rhs.pretty}"
-    case Bool(b)                     => b.toString
-    case Identifier(id)              => id
-    case Integer(i)                  => i.toString
-    case Application(left, right)    => s"(${left.pretty}) ${right.pretty}"
-    case LambdaAbstraction(id, body) => s"${id.pretty} . ${body.pretty}"
-    case Nil                         => ""
+    def pretty: String = this match {
+      case Named(lhs, rhs)             => s"${lhs.pretty} = ${rhs.pretty}"
+      case Bool(b)                     => b.toString
+      case Identifier(id)              => id
+      case Integer(i)                  => i.toString
+      case Application(left, right)    => s"(${left.pretty}) ${right.pretty}"
+      case LambdaAbstraction(id, body) => s"${id.pretty} . ${body.pretty}"
+      case Empty                         => ""
+    }
+
   }
 
+  sealed trait Declaration extends Fragment
+  sealed trait Term        extends Fragment
+
+  case class Named(lhs: Identifier, rhs: Term) extends Declaration
+
+  case class Bool(b: Boolean)                        extends Term
+  case class Identifier(id: String)                  extends Term
+  case class Floating(f: Float)                      extends Term
+  case class Integer(i: Int)                         extends Term
+  case class Application(left: Term, right: Term)    extends Term
+  case class LambdaAbstraction(id: Term, body: Term) extends Term
+
+  case object Empty extends Term
 }
 
-sealed trait Declaration extends Fragment
-sealed trait Term        extends Fragment
-
-case class Named(lhs: Identifier, rhs: Term) extends Declaration
-
-case class Bool(b: Boolean)                        extends Term
-case class Identifier(id: String)                  extends Term
-case class Floating(f: Float)                      extends Term
-case class Integer(i: Int)                         extends Term
-case class Application(left: Term, right: Term)    extends Term
-case class LambdaAbstraction(id: Term, body: Term) extends Term
-
-case object Nil extends Term
-
 object ParserExample extends App {
+  import it.vigtig.lambda.syntax.AST._
 
   val programString =
     """|x = 20
       |* ((a . b . + a b) x 1) 2""".stripMargin
 
-  val ast = FraseLexer(programString).flatMap(Parser.apply)
+  val ast = DefaultParser(DefaultLexer()).parse(programString)
 
   println(ast.map(_.mkString("\n")))
   println(ast.map(_.map(_.pretty).mkString("\n")))
 
 }
 
-class Parser extends Parsers with PackratParsers {
+trait Parser {
+  def parse(program: String): Either[String, List[Fragment]]
+}
+
+case class DefaultParser(lexer: Lexer) extends Parser with Parsers with PackratParsers {
 
   override type Elem = Token
 
-  def apply(tokens: List[Token]): Either[String, List[Fragment]] = {
+  def parse(program: String): Either[String, List[Fragment]] =
+    lexer.tokenize(program).flatMap(parse)
+
+  private def parse(tokens: List[Token]): Either[String, List[Fragment]] = {
     val reader = new PackratReader(TokenReader(tokens))
     program(reader) match {
       case NoSuccess(msg, _)  => Left(msg)
@@ -56,19 +68,19 @@ class Parser extends Parsers with PackratParsers {
     }
   }
 
-  lazy val fragment: PackratParser[Fragment] = (named | term) <~ NEWLINE.*
+  private lazy val fragment: PackratParser[Fragment] = (named | term) <~ NEWLINE.*
 
-  lazy val program: PackratParser[List[Fragment]] = phrase(rep1(fragment))
+  private lazy val program: PackratParser[List[Fragment]] = phrase(rep1(fragment))
 
-  case class MyPos(line: Int, column: Int, lineContents: String) extends Position
-  case class TokenReader(tokens: List[Token], position: Int = 1) extends Reader[Token] {
+  private case class MyPos(line: Int, column: Int, lineContents: String) extends Position
+  private case class TokenReader(tokens: List[Token], position: Int = 1) extends Reader[Token] {
     override def first: Token        = tokens.head
     override def rest: Reader[Token] = TokenReader(tokens.drop(1), position + 1)
     override def pos: Position       = MyPos(1, position, tokens.mkString)
     override def atEnd: Boolean      = tokens.isEmpty
   }
 
-  lazy val application: PackratParser[Term] = term ~ (pterm | terminal) ^^ {
+  private lazy val application: PackratParser[Term] = term ~ (pterm | terminal) ^^ {
     case left ~ right => Application(left, right)
   }
 
@@ -76,23 +88,23 @@ class Parser extends Parsers with PackratParsers {
     case id ~ body => LambdaAbstraction(id, body)
   }
 
-  lazy val term: PackratParser[Term] = lambdaAbstraction | application | pterm | terminal
+  private lazy val term: PackratParser[Term] = lambdaAbstraction | application | pterm | terminal
 
-  lazy val terminal: PackratParser[Term] = integer | identifier
+  private lazy val terminal: PackratParser[Term] = integer | identifier
 
-  lazy val pterm: PackratParser[Term] = `(` ~> term <~ `)`
+  private lazy val pterm: PackratParser[Term] = `(` ~> term <~ `)`
 
-  lazy val integer: PackratParser[Integer] =
+  private lazy val integer: PackratParser[Integer] =
     accept("integer literal", {
       case INTGR(str) => Integer(str.toInt)
     })
 
-  lazy val identifier: PackratParser[Identifier] =
+  private lazy val identifier: PackratParser[Identifier] =
     accept("identifier", {
       case ID(str) => Identifier(str)
     })
 
-  lazy val named: PackratParser[Named] = identifier ~ (`=` ~> term) ^^ {
+  private lazy val named: PackratParser[Named] = identifier ~ (`=` ~> term) ^^ {
     case id ~ term => Named(id, term)
   }
 
